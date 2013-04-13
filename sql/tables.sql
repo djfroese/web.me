ad0c604ccf602ba577a2ed7ea418027509748dbd
David Froese <david.froese@gmail.com>
2013-04-13 18:03:46 -0500

    added Users table to sql/tables.sql and ran created the table in the database

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,9 +1,18 @@
 CREATE TABLE Posts (
-	id integer primary key asc autoincrement,
-	title text,
-	body text,
-	created datetime,
-	modified datetime
+id integer primary key asc autoincrement,
+title text,
+body text,
+created datetime,
+modified datetime
 );
 
-	
+CREATE TABLE Users (
+id integer primary key asc autoincrement,
+username text unique,
+pw text,
+email text unique,
+firstname text,
+lastname text,
+created datetime,
+modifited datetime
+);
